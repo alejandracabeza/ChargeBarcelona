44227c60ac76846535f05465b44eb146fda23bbd
unknown <alejandrapcabeza@gmail.com>
2019-08-19 17:48:18 +0200

    Fix app and add fetch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import ReactMapGL from "react-map-gl";
-// import Map from "./components/map";
+import Map from "./components/map";
 
 function App() {
-  const [viewport, setViewport] = useState({
-    latitute: 38.889931,
-    longitude: -77.009003,
-    zoom: 10,
-    height: "100vh",
-    width: "100vw"
-  });
+  // let [latitude, setLatitude] = useState();
+  // let [longitude, setLongitude] = useState();
+
+  // async function fetchJSON() {
+  //   try {
+  //     const response = await fetch(
+  //       "https://api.bsmsa.eu/ext/api/bsm/chargepoints/v1/chargepoints"
+  //     );
+  //     const json = await response.json();
+  //     const temp = json.result.chargepoint;
+  //     for (let i = 0; i < temp.length; i++) {
+  //       temp.map(location => {
+  //         let latitude = location.lat;
+  //         let longitude = location.lng;
+  //       });
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   fetchJSON();
+  // }, []);
 
   return (
     <div className="map">
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/alejandrapcabeza/cjzcj8g430ajf1cp0fr7odmz5"
-        onViewportChange={viewport => {
-          setViewport(viewport);
-        }}
-      >
-        {" "}
-        markers here
-      </ReactMapGL>
+      <Map />
     </div>
   );
 }
--- src/components/map.js
+++ src/components/map.js
@@ -1,15 +1,50 @@
-// import React from "react";
-// import "./../App.css";
-// import ReactMapGL from "react-map-gl";
+import React, { useState, useEffect } from "react";
+import "./../App.css";
+import ReactMapGL, { Marker } from "react-map-gl";
 
-// function Map({ mapboxApiAccessToken, viewport }) {
-//   console.log(mapboxApiAccessToken);
-//   console.log(viewport);
-//   return (
-//     <div className="Map">
+function Map() {
+  let [marker, setMarker] = useState({});
+  const [viewport, setViewport] = useState({
+    longitude: 2.154007,
+    latitude: 41.390205,
+    zoom: 12,
+    height: "100vh",
+    width: "100vw"
+  });
 
-//     </div>
-//   );
-// }
+  async function fetchJSON() {
+    try {
+      const response = await fetch(
+        "https://api.bsmsa.eu/ext/api/bsm/chargepoints/v1/chargepoints"
+      );
+      const json = await response.json();
+      const temp = json.result.chargepoint;
+      for (let i = 0; i < temp.length; i++) {
+        // temp.map(location => {
+        //   return setMarker(location);
+        // });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  console.log(marker);
+  useEffect(() => {
+    fetchJSON();
+  }, []);
 
-// export default Map;
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/alejandrapcabeza/cjzcj8g430ajf1cp0fr7odmz5"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      />
+    </div>
+  );
+}
+
+export default Map;
